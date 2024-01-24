63f0d10515001ded6aed810a5dc63cb70d6cb7c2
atanu <atanurkm11@gmail.com>
2024-01-24 15:10:44 -0700

    clusterStatistics

--- clusterStatistics.m
+++ clusterStatistics.m
@@ -1,9 +1,9 @@
 % Author: Atanu Giri
 % Date: 01/22/2024
 
-% function clusterStatistics(filename)
+function clusterStatistics(filename)
 
-filename = 'Baseline DT_1.xlsx';
+% filename = 'Baseline DT_1.xlsx';
 
 % Use regular expression to extract the desired part
 expression = ' (\w+)_';
@@ -28,6 +28,8 @@ for baselineCluster = 1:numClusters
     BLtable = readtable(baselineFilePath);
     BLdata = [BLtable.clusterX, BLtable.clusterY];
     BLDataMean = mean(BLdata);
+%     stdDevBLx = std(BLdata(:,1))/sqrt(length(BLdata(:,1)));
+%     stdDevBLy = std(BLdata(:,2))/sqrt(length(BLdata(:,2)));
 
     % Get a list of all files in the folder
     allFiles = dir(fullfile(folderPath, '*.xlsx'));
@@ -36,7 +38,10 @@ for baselineCluster = 1:numClusters
     featureFiles = allFiles(contains({allFiles.name}, feature) & ~contains({allFiles.name}, 'Baseline'));
 
     % Placeholder for cluster data
+    healthData = cell(1, length(featureFiles));
     healthDataMean = cell(1, length(featureFiles));
+    healthDataSDx = zeros(1, length(featureFiles));
+    healthDataSDy = zeros(1, length(featureFiles));
 
     % Compare Baseline data with each health group file
     for index = 1:length(featureFiles)
@@ -45,36 +50,47 @@ for baselineCluster = 1:numClusters
 
         % Load data from health group Excel file
         healthTable = readtable(healthFilePath);
-        healthData = [healthTable.clusterX, healthTable.clusterY];
-        healthDataMean{index} = mean(healthData);
+        healthData{index} = [healthTable.clusterX, healthTable.clusterY];
+        healthDataMean{index} = mean(healthData{index});
+        healthDataSDx(index) = std(healthData{index}(:,1));
+        healthDataSDy(index) = std(healthData{index}(:,2));
 
-        [hX, pX] = vartest2(BLdata(:,1), healthData(:,1));
-        [hY, pY] = vartest2(BLdata(:,2), healthData(:,2));
+
+%         [hX, pX] = vartest2(BLdata(:,1), healthData(:,1));
+%         [hY, pY] = vartest2(BLdata(:,2), healthData(:,2));
 
         if isequal(mod(index, 3), 0)
             healthFileName = featureFiles(index).name;
             healthFileNameExp = '^(.*?)(?=\d)';
             healthName = regexp(healthFileName, healthFileNameExp, 'match', 'once');
  
-            % Save result
-            h = figure;
+            % Plot centroids
+            figure;
             hold on;
             title(sprintf("Comparison between BL %s_Cluster%d and %s", ...
                 feature, baselineCluster, healthName), 'Interpreter','none');
-            Colors = spring(4);
+            Colors = lines(4);
             plot(BLDataMean(1), BLDataMean(2), 'Color', ...
-                    Colors(4,:), 'LineStyle','none', 'Marker','.','MarkerSize',50);
+                    Colors(4,:), 'LineStyle','none', 'Marker','.','MarkerSize',40);
+
+            error_ellipse_fun(BLdata, 0.95);
+%             h.EdgeColor = Colors(4,:);
 
             for i = index-2:index
                 plot(healthDataMean{i}(1),healthDataMean{i}(2),'Color', ...
-                    Colors(mod(i, 3)+1,:), 'LineStyle','none', 'Marker','.','MarkerSize',50);
+                    Colors(mod(i, 3)+1,:), 'LineStyle','none', 'Marker','.','MarkerSize',40);
+                error_ellipse_fun(healthData{i}, 0.95);
+%                 h.EdgeColor = Colors(mod(i, 3)+1,:);
+
             end
+
             xlim([-20, 20]); ylim([-20, 20])
-            legend('','Cluster1','Cluster2','Cluster3');
+            legend;
+%             legend('','Cluster1','Cluster2','Cluster3');
             hold off;
 
         end
     end
 end
 
-% end
+end % end of function
